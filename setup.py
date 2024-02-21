e5962d76679670a0457ec98f99d6de2b935e146a
faradox <felix@okio.ai>
2024-02-21 10:13:46 +0100

    chore: prepare release 0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 if __name__ == "__main__":
     setup(
         name="nendo-plugin-loopify",
-        version="0.1.5",
+        version="0.1.6",
         description="Nendo loopifier plugin",
         author="Okio Team <team@okio.ai>",
     )
